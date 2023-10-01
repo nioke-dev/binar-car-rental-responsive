bc914298212bb609f6a5830f8a588ec2cc0b3b13
“nioke-dev” <nioke8090@gmail.com>
2023-10-01 22:56:11 +0700

    fixing bug navbar active item

--- script.js
+++ script.js
@@ -40,3 +40,30 @@ $(document).ready(function () {
     }
   });
 });
+
+// 1. tangkap element dengan class menu
+const menu = document.querySelector(".navbar-nav");
+
+// 2. jika element dengan class menu diklik
+menu.addEventListener("click", function (e) {
+  // 3. maka ambil element apa yang diklik oleh user
+  const targetMenu = e.target;
+
+  // 4. lalu cek, jika element itu adalah link dengan class menu__link
+  if (targetMenu.classList.contains("nav-link")) {
+    // 5. maka ambil menu link yang aktif
+    const menuLinkActive = document.querySelector("ul li a.active");
+
+    // 6. lalu cek, Jika menu link active ada dan menu yang di klik user adalah menu yang tidak sama dengan menu yang aktif, (cara cek-nya yaitu dengan membandingkan value attribute href-nya)
+    if (
+      menuLinkActive !== null &&
+      targetMenu.getAttribute("href") !== menuLinkActive.getAttribute("href")
+    ) {
+      // 7. maka hapus class active pada menu yang sedang aktif
+      menuLinkActive.classList.remove("active");
+    }
+
+    // 8. terakhir tambahkan class active pada menu yang di klik oleh user
+    targetMenu.classList.add("active");
+  }
+});
